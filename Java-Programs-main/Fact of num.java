4d855ba267ad75336cadc9d38b8d2d23d8e4b2dd
Farmaan E illahi Khan <89920869+khanfarmaan@users.noreply.github.com>
2021-10-22 10:56:31 +0530

    Add files via upload

---
+++ Java-Programs-main/Fact of num.java
@@ -0,0 +1,11 @@
+public class numbers {
+    public static void main(String[] args) {
+        int number = 6;
+        long fact = 1;
+        for(int i = 1; i <= number; i++)
+        {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+}
---
+++ Java-Programs-main/conditionaloperator.java
@@ -0,0 +1,8 @@
+public class Number {
+  public static void main(String args[]) {
+    int i = 69;
+    int j = 50;
+    int result = (i < j) ? i : j;
+    System.out.println(result + " is a minimum value");
+  }
+}
